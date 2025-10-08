afcdc62d01582691af6fb3cf814d42d559731cc5
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-10-08 11:04:16 +0000

    Separate sliders from progress bars

--- lib/src/buttons_controls/slider.dart
+++ lib/src/buttons_controls/slider.dart
@@ -32,25 +32,3 @@ class WebbUISlider extends StatelessWidget {
     );
   }
 }
-
-class WebbUIProgressBar extends StatelessWidget {
-  final double value; // 0.0 to 1.0
-  final bool isIndeterminate;
-
-  const WebbUIProgressBar({
-    super.key,
-    required this.value,
-    this.isIndeterminate = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final webbTheme = context;
-    return LinearProgressIndicator(
-      value: isIndeterminate ? null : value,
-      backgroundColor: webbTheme.colorPalette.neutralLight.withOpacity(0.3),
-      valueColor: AlwaysStoppedAnimation<Color>(webbTheme.colorPalette.primary),
-      minHeight: 4.0,
-    );
-  }
-}
