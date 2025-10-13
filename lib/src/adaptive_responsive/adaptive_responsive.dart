aff0873d933f3f8af2899f162db1261ab1425dca
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-10-13 17:22:00 +0000

    Update Adaptive Responsive module

--- lib/src/adaptive_responsive/adaptive_responsive.dart
+++ lib/src/adaptive_responsive/adaptive_responsive.dart
@@ -1,2 +1,3 @@
 export 'responsive_grid.dart';
 export 'breakpoint_layout.dart';
+export 'responsive_builder.dart';
--- lib/src/adaptive_responsive/responsive_grid.dart
+++ lib/src/adaptive_responsive/responsive_grid.dart
@@ -4,44 +4,51 @@ import 'package:webb_ui/src/theme.dart';
 
 class WebbUIResponsiveGrid extends StatelessWidget {
   final List<Widget> children;
-  final int? crossAxisCount;
+  final int? mobileCrossAxisCount;
+  final int? tabletCrossAxisCount;
+  final int? desktopCrossAxisCount;
   final double? childAspectRatio;
   final EdgeInsets? padding;
+  final double mainAxisSpacing;
+  final double crossAxisSpacing;
 
   const WebbUIResponsiveGrid({
     super.key,
     required this.children,
-    this.crossAxisCount,
+    this.mobileCrossAxisCount,
+    this.tabletCrossAxisCount,
+    this.desktopCrossAxisCount,
     this.childAspectRatio,
     this.padding,
+    this.mainAxisSpacing = 16.0,
+    this.crossAxisSpacing = 16.0,
   });
 
   @override
   Widget build(BuildContext context) {
     final webbTheme = context;
     final double width = MediaQuery.of(context).size.width;
-    final int effectiveCrossAxisCount = crossAxisCount ??
-        (width >= WebbUIBreakpoints.desktop
-            ? 4
-            : width >= WebbUIBreakpoints.tablet
-                ? 2
-                : 1); // Desktop: 4, Tablet: 2, Mobile: 1
+
+    int getCrossAxisCount() {
+      if (WebbUIBreakpoints.isMobile(width)) {
+        return mobileCrossAxisCount ?? 1;
+      } else if (WebbUIBreakpoints.isTablet(width)) {
+        return tabletCrossAxisCount ?? 2;
+      } else {
+        return desktopCrossAxisCount ?? 4;
+      }
+    }
 
     return Padding(
       padding: padding ?? EdgeInsets.all(webbTheme.spacingGrid.spacing(2)),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return GridView.count(
-            crossAxisCount: effectiveCrossAxisCount,
-            childAspectRatio: childAspectRatio ?? 1.0,
-            mainAxisSpacing: webbTheme.spacingGrid.gutter,
-            crossAxisSpacing: webbTheme.spacingGrid.gutter,
-            shrinkWrap: true,
-            physics:
-                const NeverScrollableScrollPhysics(), // Parent handles scrolling
-            children: children,
-          );
-        },
+      child: GridView.count(
+        crossAxisCount: getCrossAxisCount(),
+        childAspectRatio: childAspectRatio ?? 1.0,
+        mainAxisSpacing: mainAxisSpacing,
+        crossAxisSpacing: crossAxisSpacing,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        children: children,
       ),
     );
   }
