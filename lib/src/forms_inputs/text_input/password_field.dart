828e8c55cce254efdfa405a8fff96c8176009ee8
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-10-19 14:32:37 +0000

    Update Password field

--- lib/src/forms_inputs/text_input/password_field.dart
+++ lib/src/forms_inputs/text_input/password_field.dart
@@ -5,20 +5,28 @@ import 'validation_states.dart';
 
 class WebbUIPasswordField extends StatefulWidget {
   final TextEditingController? controller;
+  final FocusNode? focusNode;
   final String? label;
   final String? hintText;
   final WebbUIValidationState validationState;
   final String? validationMessage;
   final bool disabled;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+  final ValueChanged<String>? onSubmitted;
 
   const WebbUIPasswordField({
     super.key,
     this.controller,
+    this.focusNode,
     this.label,
     this.hintText,
     this.validationState = WebbUIValidationState.none,
     this.validationMessage,
     this.disabled = false,
+    this.autofocus = false,
+    this.onChanged,
+    this.onSubmitted,
   });
 
   @override
@@ -26,44 +34,84 @@ class WebbUIPasswordField extends StatefulWidget {
 }
 
 class _WebbUIPasswordFieldState extends State<WebbUIPasswordField> {
+  late FocusNode _focusNode;
+  late TextEditingController _controller;
   bool _obscureText = true;
+  bool _hasFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = widget.focusNode ?? FocusNode();
+    _controller = widget.controller ?? TextEditingController();
+
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void dispose() {
+    if (widget.focusNode == null) {
+      _focusNode.dispose();
+    }
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _handleFocusChange() {
+    setState(() {
+      _hasFocus = _focusNode.hasFocus;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final webbTheme = context;
 
-    // 1. Define the custom suffix icon (visibility toggle)
+    // Visibility toggle icon
     final Widget suffixIcon = IconButton(
       icon: Icon(
         _obscureText ? Icons.visibility_off : Icons.visibility,
         size: webbTheme.iconTheme.mediumSize,
+        color: widget.disabled
+            ? webbTheme.interactionStates.disabledColor
+            : webbTheme.colorPalette.neutralDark,
       ),
       onPressed: widget.disabled
-          ? null // Disable toggle if the field is disabled
+          ? null
           : () => setState(() => _obscureText = !_obscureText),
-      color: webbTheme.colorPalette.neutralDark,
       splashRadius: webbTheme.iconTheme.mediumSize / 2,
     );
 
-    // 2. Generate the theme-consistent decoration using the utility class's getDecoration() method.
-    final InputDecoration decoration = WebbUIInputDecoration(
+    final decoration = WebbUIInputDecoration(
       webbTheme: webbTheme,
       label: widget.label,
       hintText: widget.hintText,
+      suffixIcon: suffixIcon,
       validationState: widget.validationState,
       validationMessage: widget.validationMessage,
-      suffixIcon: suffixIcon, // Inject the custom suffix icon here
-      maxLines: 1, // Always 1 for password fields
+      maxLines: 1,
+      isFocused: _hasFocus,
+      isDisabled: widget.disabled,
     ).getDecoration();
 
     return TextFormField(
-      controller: widget.controller,
+      controller: _controller,
+      focusNode: _focusNode,
       obscureText: _obscureText,
       enabled: !widget.disabled,
+      autofocus: widget.autofocus,
       keyboardType: TextInputType.visiblePassword,
+      onChanged: widget.onChanged,
+      onFieldSubmitted: widget.onSubmitted,
+      textInputAction: TextInputAction.done,
       decoration: decoration,
-      style: webbTheme.typography.bodyMedium
-          .copyWith(color: webbTheme.colorPalette.neutralDark),
+      style: webbTheme.typography.bodyMedium.copyWith(
+        color: widget.disabled
+            ? webbTheme.interactionStates.disabledColor
+            : webbTheme.colorPalette.neutralDark,
+      ),
     );
   }
 }
