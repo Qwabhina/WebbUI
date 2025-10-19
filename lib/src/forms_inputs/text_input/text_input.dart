0b0f2d716370877b3c8794525c71d996175d6626
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-10-19 14:33:02 +0000

    Update Barrel File for Text Inputs

--- lib/src/forms_inputs/text_input/text_input.dart
+++ lib/src/forms_inputs/text_input/text_input.dart
@@ -1,2 +1,5 @@
 export 'text_field.dart' show WebbUITextField;
-export 'password_field.dart';
+export 'password_field.dart' show WebbUIPasswordField;
+export 'editable_text_field.dart' show WebbUIEditableTextField;
+export 'input_decoration.dart' show WebbUIInputDecoration;
+export 'validation_states.dart' show WebbUIValidationState;
