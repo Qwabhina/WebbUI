f10d4ef1541a70253dd638117b9bce88d3a689f5
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-10-20 11:57:37 +0000

    Update Parent Barrel file

--- lib/src/forms_inputs/forms_inputs.dart
+++ lib/src/forms_inputs/forms_inputs.dart
@@ -1,4 +1,4 @@
 export 'text_input/text_input.dart';
 export 'date_time_picker/date_time_picker.dart';
 export 'number_stepper.dart' show WebbUINumberStepper;
-export 'file_upload.dart' show WebbUIFileUpload;
+export 'file_upload/file_upload.dart' show WebbUIFileUpload;
