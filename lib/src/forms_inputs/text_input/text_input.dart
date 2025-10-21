dddf9915d662cf082839391d12203cef527d99bf
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-10-21 22:08:04 +0000

    Rename text inputs

--- lib/src/forms_inputs/text_input/text_input.dart
+++ lib/src/forms_inputs/text_input/text_input.dart
@@ -1,5 +1,5 @@
 export 'textfield.dart' show WebbUITextField;
 export 'password_textfield.dart' show WebbUIPasswordField;
-export 'editable_text_field.dart' show WebbUIEditableTextField;
+export 'editable_textfield.dart' show WebbUIEditableTextField;
 export 'input_decoration.dart' show WebbUIInputDecoration;
 export 'validation_states.dart' show WebbUIValidationState;
