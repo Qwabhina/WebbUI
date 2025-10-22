1ff7c08223d45b04f6d5646b87fa25aaf2fdd269
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-10-22 15:52:09 +0000

    Update Chart Barrel file

--- lib/src/content_containers/chart/chart.dart
+++ lib/src/content_containers/chart/chart.dart
@@ -1,3 +1,21 @@
-
+// Main exports
 export 'chart_widget.dart' show WebbUIChart;
-export 'chart_definitions.dart';
+export 'chart_definitions.dart'
+    show
+        ChartData,
+        ChartSeries,
+        ChartType,
+        AxisType,
+        LegendPosition,
+        LegendAlignment,
+        TooltipBehavior,
+        ChartConfig,
+        ChartBounds;
+
+// Painter exports for advanced usage
+export 'chart_painter.dart' show WebbUIChartPainter;
+export 'chart_coordinate_system.dart' show ChartCoordinateSystem;
+export 'chart_grid_painter.dart' show ChartGridPainter;
+export 'chart_series_painter.dart'
+    show ChartSeriesPainter, ChartSeriesPainterFactory;
+export 'chart_tooltip_painter.dart' show ChartTooltipPainter;
